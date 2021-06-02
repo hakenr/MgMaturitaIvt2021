d3eb748d7e75c4a5c62df656013155c00ed15a3c
Robert Haken <haken@havit.cz>
2021-06-03 00:16:27 +0200

    Add project files.

---
+++ BmpPlechovka/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BmpPlechovka
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var recipe = ReadRecipe();
+
+			Console.Write(recipe);
+		}
+
+		private static Recipe ReadRecipe()
+		{
+			var recipe = new Recipe();
+			recipe.InputFilePath = Console.ReadLine();
+			recipe.OutputFilePath = Console.ReadLine();
+
+			var startLine = Console.ReadLine();
+			var startLineItems = startLine.Split(' ');
+			recipe.StartX = int.Parse(startLineItems[0]);
+			recipe.StartY = int.Parse(startLineItems[1]);
+
+			var targetLine = Console.ReadLine();
+			var targetLineItems = targetLine.Split(' ');
+			recipe.TargetR = int.Parse(targetLineItems[0]);
+			recipe.TargetG = int.Parse(targetLineItems[1]);
+			recipe.TargetB = int.Parse(targetLineItems[2]);
+
+			var sensitivityLine = Console.ReadLine();
+			recipe.Sensitivity = int.Parse(sensitivityLine);
+
+			return recipe;
+		}
+	}
+}
---
+++ BmpPlechovka/Recipe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmpPlechovka
+{
+    public record Recipe
+    {
+		public string InputFilePath { get; set; }
+		public string OutputFilePath { get; set; }
+
+		public int StartX { get; set; }
+		public int StartY { get; set; }
+
+		public int TargetR { get; set; }
+		public int TargetG { get; set; }
+		public int TargetB { get; set; }
+
+		public int Sensitivity { get; set; }
+	}
+}
